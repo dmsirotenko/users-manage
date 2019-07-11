381be84e8afccd011fe7f42105fe4f8f6892f361
Дмитрий <sirotenkod@zeptolab.com>
2019-07-12 02:42:18 +0400

    User fields

--- src/main/java/com/sirotenkod/test/usersmanage/dao/UserDAO.java
+++ src/main/java/com/sirotenkod/test/usersmanage/dao/UserDAO.java
@@ -1,4 +1,39 @@
 package com.sirotenkod.test.usersmanage.dao;
 
+import javax.persistence.*;
+
+@Entity
 public class UserDAO {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
--- src/main/java/com/sirotenkod/test/usersmanage/dto/UserDTO.java
+++ src/main/java/com/sirotenkod/test/usersmanage/dto/UserDTO.java
@@ -1,4 +1,36 @@
 package com.sirotenkod.test.usersmanage.dto;
 
+import javax.validation.constraints.NotBlank;
+
 public class UserDTO {
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
